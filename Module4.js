239962e51d1d3a0e727a18a3134aae46ef8ef58a
Anasasiia Senchenko <anastasiya.sencjenko@gmail.com>
2023-02-10 17:33:01 +0500

    Conditional statements and Data types Practical task

---
+++ Module4.js
@@ -0,0 +1,19 @@
+//Perform addition of various types
+console.log("1" + false); //string + boolean
+console.log("2px" + 1); //string + number
+console.log(1 + true); //number + boolean
+
+//Perform multiplication of various types
+console.log("5" * true); //string * boolean
+console.log("3" * 3); //string * number
+console.log(5 * true); //number * boolean
+
+//Divide different types 
+console.log("5px" / false); //string / boolean
+console.log("8" / 2); //string / number
+console.log(0 / true); //number / boolean
+
+//Perform explicit conversion
+console.log("10" / "2"); //number conversion
+console.log("Hello" + 1); //string conversion
+console.log(Boolean("Hello"));// boolean conversion
