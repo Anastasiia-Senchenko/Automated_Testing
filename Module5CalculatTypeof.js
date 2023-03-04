e7e743dcea459766c67d500b8006d076bdac3e43
Anasasiia Senchenko <anastasiya.sencjenko@gmail.com>
2023-03-04 15:05:40 +0500

    Edit calult Typeof

---
+++ Module5CalculatTypeof.js
@@ -0,0 +1,9 @@
+//Write a function for calculating the type of argument and type output to the console 
+
+function calculatType(myParamType) {
+  console.log(`type of input is ${typeof myParamType}`)
+}
+
+calculatType(33);
+calculatType("It is a string");
+calculatType(true);
